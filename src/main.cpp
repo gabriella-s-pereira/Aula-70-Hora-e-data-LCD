fc3717fa547e421c9bfb38c927549abb40ee46bd
gabriella-s-pereira <gabriella.s.pereira@aluno.senai.br>
2025-05-09 17:05:57 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-#include "internet.h"
 #include <ezTime.h>
+#include "internet.h"
 #include <LiquidCrystal_I2C.h>
 #include <Bounce2.h>
 
@@ -11,40 +11,41 @@ Bounce botaoBoot = Bounce();
 int dia = 0;
 int mes = 0;
 int ano = 0;
-int hora = 0; 
-int minutos[2] = {0, 0};
-int segundos[2] = {0, 0};
+int hora = 0;
+int minuto[2] = {0, 0};
+int segundo[2] = {0, 0};
 int diaSemana = 0;
 int diaAno = 0;
-int formato24h = true;
+bool formato24h = true;
 bool manha = false;
 
-const char *diaSemanaTexto[] ={
-"Domingo",
-"segunda",
-"Terca",
-"Quarta",
-"Quinta", 
-"sexta",
-"sabado"};
+const char *diaSemanaTexto[] = {
+    "Domingo",
+    "Segunda",
+    "Terca",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sabado"};
 
 
 const char *mesTexto[] = {
-"jan"
-"fev"
-"mar"
-"abr"
-"mai"
-"jun"
-"jul"
-"ago"
-"set"
-"out"
-"nov"
-"dez"
+  "jan",
+  "fev",
+  "mar",
+  "abr",
+  "mai",
+  "jun",
+  "jul",
+  "ago",
+  "set",
+  "out",
+  "nov",
+  "dez"
 };
- 
- void display(void);
+
+
+void display(void);
 
 void setup()
 {
@@ -57,9 +58,9 @@ void setup()
   dia = tempo.day();
   mes = tempo.month();
   ano = tempo.year();
-  hora = formato24h ? tempo.hour() : tempo.hourFormat12();
-  minutos[1] = tempo.minute();
-  segundos[1] = tempo.second();
+  hora = tempo.hour();
+  minuto[1] = tempo.minute();
+  segundo[1] = tempo.second();
   diaSemana = tempo.weekday();
   diaAno = tempo.dayOfYear();
   manha = tempo.isAM();
@@ -68,66 +69,68 @@ void setup()
   lcd.backlight();
   display();
 }
+
 void loop()
 {
   botaoBoot.update();
 
-  if( botaoBoot.fell())
+  if (botaoBoot.fell())
   {
-formato24h = !formato24h;
-  if (!formato24h)
-  {
-    lcd.setCursor(9,2);
-    lcd.printf("%s",manha ? "AM" : "PM");
-    lcd.setCursor(0,2);
-    lcd.printf("%02d",tempo.hourFormat12());
+    formato24h = !formato24h;
 
+    if (!formato24h)
+    {
+      lcd.setCursor(9, 2);
+      lcd.printf("%s", manha ? "AM" : "PM");
+      lcd.setCursor(0, 2);
+      lcd.printf("%02d", tempo.hourFormat12());
+    }
+
+    else
+    {
+      lcd.setCursor(9, 2);
+      lcd.print("  ");
+      lcd.setCursor(0, 2);
+      lcd.printf("%02d", tempo.hour());
+    }
   }
-  else 
-  {
-    lcd.setCursor(9,2);
-    lcd.print("   ");
-    lcd.setCursor(0,2);
-    lcd.printf("%02d", tempo.hour());
-  }
-  }
-  void checkWiFi();
-  segundos[1] = tempo.second();
-  minutos[1] = tempo.minute();
-  if (segundos[1] != segundos[0])
-   
+
+  checkWiFi();
+  segundo[1] = tempo.second();
+  minuto[1] = tempo.minute();
+
+  if (segundo[1] != segundo[0])
   {
     lcd.setCursor(6, 2);
-    lcd.printf("%20d", segundos);
-    segundos[0] = segundos[1];
+    lcd.printf("%02d", segundo[1]);
+    segundo[0] = segundo[1];
   }
 
-  if (minutos[0] == 59 && minutos[0])
+  if (minuto[0] == 59 && minuto[1] == 0)
   {
-
     dia = tempo.day();
     mes = tempo.month();
     ano = tempo.year();
-    hora = tempo.hour();
+    hora = formato24h ? tempo.hour() : tempo.hourFormat12();
     diaSemana = tempo.weekday();
     diaAno = tempo.dayOfYear();
     manha = tempo.isAM();
+
     display();
   }
-  if (minutos[1] != minutos[0])
-    
+
+  if (minuto[1] != minuto[0])
   {
     lcd.setCursor(3, 2);
-    lcd.printf("%02d", minutos[1]);
-    minutos[0]= minutos[1];
+    lcd.printf("%02d", minuto[1]);
+    minuto[0] = minuto[1];
+  }
 }
-}
+
 void display()
 {
-  
-  
   lcd.setCursor(0, 0);
-  lcd.printf("%02d/%s/%04d", dia, mesTexto[mes-1], ano);
+  lcd.printf("%02d/%s/%04d", dia, mesTexto[mes - 1], ano);
 
   lcd.setCursor(16, 0);
   lcd.printf("%03d", diaAno);
@@ -136,6 +139,6 @@ void display()
   lcd.print(diaSemanaTexto[diaSemana - 1]);
 
   lcd.setCursor(0, 2);
-  lcd.printf("%02d:%02d:%02d", hora, minutos[1], segundos[1]);
+  lcd.printf("%02d:%02d:%02d", hora, minuto[1], segundo[1]);
 }
   
